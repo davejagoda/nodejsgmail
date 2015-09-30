6c7f2abaf6df38ce80aba6743c0a11995320bc89
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-09-30 16:26:04 -0700

    rewrite mail.js to make imap connection inside xoauth2gen.getToken

--- mail.js
+++ mail.js
@@ -1,34 +1,49 @@
 var Imap = require('imap'),
     inspect = require('util').inspect;
 
-var imap = new Imap({
-  //  debug: console.log,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  host: 'imap.gmail.com',
-  port: 993,
-  tls: true,
-  keepAlive: false
+var xoauth2 = require("xoauth2"),
+    xoauth2gen;
+
+xoauth2gen = xoauth2.createXOAuth2Generator({
+  user: process.env.EMAIL,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  refreshToken: process.env.REFRESH_TOKEN
 });
 
-function openInbox(cb) {
-  imap.openBox('INBOX', true, cb);
-}
-
-imap.once('ready', function() {
-  openInbox(function(err, box) {
-    if (err) throw err;
-    console.log(box.messages);
-    imap.end();
+xoauth2gen.getToken(function(err, token) {
+  if(err){
+    return console.log(err);
+  }
+  var imap = new Imap({
+	  //    debug: console.log,
+    xoauth2: token,
+    host: 'imap.gmail.com',
+    port: 993,
+    tls: true,
+    keepAlive: false
   });
-});
 
-imap.once('error', function(err) {
-  console.log(err);
-});
+  function openInbox(cb) {
+    imap.openBox('INBOX', true, cb);
+  }
 
-imap.once('end', function() {
-  console.log('Connection ended');
-});
+  imap.once('ready', function() {
+    openInbox(function(err, box) {
+      if (err) throw err;
+      console.log(box.messages);
+      imap.end();
+    });
+  });
 
-imap.connect();
+  imap.once('error', function(err) {
+    console.log(err);
+  });
+
+  imap.once('end', function() {
+    console.log('Connection ended');
+  });
+
+  //  console.log(imap['_config']);
+  imap.connect();
+});
